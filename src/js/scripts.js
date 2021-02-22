a1ea8349c183912d0a964ea11d20402459f97682
egiorganam <giorgana.elih@gmail.com>
2021-02-22 10:47:50 -0600

    button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,14 +4,14 @@
   $(function () {
     // CarClub JS
     
-    const checkbox = document.getElementById('drop');
-    const nav = document.getElementById('ccnav');
+    const checkbox = document.getElementById('drop')
+    const nav = document.getElementById('ccnav')
     
     checkbox.addEventListener('change', () => {
-      nav.classList.toggle('is-active');
-      console.log('test');
-    });
+      nav.classList.toggle('is-active')
+      console.log('test')
+    })
 
-  });
+  })
 
 })(jQuery, window, document);
