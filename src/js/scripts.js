b4507406ac35957755a5b7229a71ea9ebbf73998
Keizyu Delgadillo <keizyu@me.com>
2021-02-23 13:59:46 -0600

    sticky header

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,9 +6,26 @@
 
     const checkbox = document.getElementById('drop');
     const nav = document.getElementById('ccnav');
+    const sticky = nav.offsetTop;
 
     checkbox.addEventListener('change', () => {
         nav.classList.toggle('is-active');
+    });
+
+    window.addEventListener('scroll', () => {
+
+        if ( window.pageYOffset >= sticky ) {
+
+            nav.classList.add('sticky');
+            nav.setAttribute('style', 'margin-top:-' + sticky + 'px');
+
+        } else {
+
+            nav.classList.remove('sticky');
+            nav.removeAttribute('style');
+
+        }
+
     });
 
 });
