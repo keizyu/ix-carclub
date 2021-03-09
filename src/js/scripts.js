2f0f57a41d970f1601cc3487c43ea5cbf8ece551
egiorganam <giorgana.elih@gmail.com>
2021-03-09 16:12:36 -0600

    add carousel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -51,4 +51,38 @@
     const three = document.getElementById("box-number-three");
     animateValue(three, 0, 80, 4000);
 
+    // CAROUSEL
+    let slidePosition = 0;
+    const slides = document.getElementsByClassName('carousel__item');
+    const totalSlides = slides.length;
+
+    document
+        .getElementById('carousel__button--next')
+        .addEventListener('click', () => moveToNextSlide())
+
+    document
+        .getElementById('carousel__button--prev')
+        .addEventListener('click', () => moveToPrevSlide())
+    
+    function updateSlidePosition(){
+        for(let slide of slides){
+            slide.classList.remove('carousel__item--visible')
+            slide.classList.add('carousel__item--hidden')
+        }
+        
+        slides[slidePosition].classList.add('carousel__item--visible')
+    }
+        
+    function moveToNextSlide(){
+        slidePosition === totalSlides - 1 ? slidePosition = 0 : slidePosition++
+        
+        updateSlidePosition()
+    }
+        
+    function moveToPrevSlide(){
+        slidePosition === 0 ? slidePosition = totalSlides - 1 : slidePosition--
+        
+        updateSlidePosition()
+    }
+
 })(window, document);
