eb3c08f3d9fcd424c82b7bc73f59925e5432b7c8
Keizyu Delgadillo <keizyu@me.com>
2021-03-25 15:49:45 -0600

    reformat

--- gulpfile.js
+++ gulpfile.js
@@ -33,41 +33,43 @@ const banner = [
 gulp.task('css', () => {
 
     return gulp.src('src/scss/style.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer('last 4 version'))
-    .pipe(gulp.dest('app/assets/css'))
-    .pipe(cssnano())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(header(banner, { package : package }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('app/assets/css'))
-    .pipe(browserSync.reload({stream:true}));
+
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 4 version'))
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(cssnano())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(header(banner, { package : package }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/assets/css'))
+        .pipe(browserSync.reload({stream:true}));
 
 });
 
 gulp.task('js', () => {
 
-  return gulp.src('src/js/scripts.js')
-    .pipe(sourcemaps.init())
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(plumber())
-    .pipe(webpack({
-        mode: 'production'
-    }))
-    .pipe(babel({
-        presets: [ '@babel/env' ]
-    }))
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('app/assets/js'))
-    .pipe(uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .pipe(header(banner, { package : package }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('app/assets/js'))
-    .pipe(browserSync.reload({stream:true, once: true}));
+    return gulp.src('src/js/scripts.js')
+
+        .pipe(sourcemaps.init())
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(plumber())
+        .pipe(webpack({
+            mode: 'production'
+        }))
+        .pipe(babel({
+            presets: [ '@babel/env' ]
+        }))
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('app/assets/js'))
+        .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(header(banner, { package : package }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('app/assets/js'))
+        .pipe(browserSync.reload({stream:true, once: true}));
 
 });
 
@@ -84,6 +86,7 @@ gulp.task('bs-reload', () => {
 });
 
 gulp.task('fileinclude', () => {
+
     gulp.src([
         'src/html/**/*.html',
         '!src/html/_components/*.html' // ignore
@@ -95,6 +98,7 @@ gulp.task('fileinclude', () => {
         // .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('app'))
         .pipe(browserSync.reload({stream:true}));
+        
 });
 
 gulp.task('start', ['css', 'js', 'browser-sync', 'fileinclude'], () => {
